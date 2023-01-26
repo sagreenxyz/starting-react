f108ab066d21f9e19461d0d09d5f9b4f142faa30
Scott A. Green <sagreenxyz@gmail.com>
2023-01-25 23:05:48 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import "./App.css"
 function App() {
   return (
     <div
-      style ={{
+      style={{
         margin: "auto",
         width: 800,
         paddingTop: "1rem"
       }}
     >
       <h1 className="title">Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <th>Name</th>
+          <th>Type</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bulbasaur</td>
+            <td>Grass, Poison</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }

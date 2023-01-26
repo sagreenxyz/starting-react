a644ae70c77ad05f16c52bea9286302194bd8312
Scott A. Green <sagreenxyz@gmail.com>
2023-01-25 23:01:41 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import "./App.css"
 
 function App() {
   return (
-    <div>
+    <div
+      style ={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem"
+      }}
+    >
       <h1 className="title">Pokemon Search</h1>
     </div>
   );
